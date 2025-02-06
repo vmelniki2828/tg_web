499029d41b237a70b0b96f73d02835d52b08880f
Vlad Melnik <vmelniki2828@gmail.com>
2025-02-07 01:17:34 +0200

    edit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,25 @@
 import { useState } from 'react';
 import Header from './Header/Header';
+import useTelegramTheme from './useTelegramTheme/useTelegramTheme';
 
 export const App = () => {
   const [asd, setAsd] = useState(false);
+  const theme = useTelegramTheme();
+
+  const appStyles = {
+    backgroundColor: theme.bgColor,
+    color: theme.textColor,
+    minHeight: '100vh',
+  };
+
+  console.log(appStyles)
 
   return (
-    <div>
+    <div style={appStyles}>
       <Header />
       <button onClick={() => setAsd(!asd)}>Нажми</button>
       {asd === true ? <>Ты пидор</> : <></>}
+      {appStyles.backgroundColor}
     </div>
   );
 };
