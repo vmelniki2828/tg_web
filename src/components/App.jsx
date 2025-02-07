2e87aa3bc7d0fd523bde7dbfc5b33e8bf2f605c8
vmelniki2828 <vmelniki2828@gmail.com>
2025-02-07 20:32:46 +0200

    edit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,28 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Header from './Header/Header';
+const tg = window.Telegram.WebApp;
 
 export const App = () => {
-  const [asd, setAsd] = useState(false);
+  // const [asd, setAsd] = useState(false);
+  // return (
+  //   <div>
+  //     <Header />
+  //     <button onClick={() => setAsd(!asd)}>Нажми</button>
+  //     {asd === true ? <>Ты пидор</> : <></>}
+  //   </div>
+  // );
+
+  useEffect(() => {
+    tg.ready();
+  }, []);
+
+  const onClose = () => {
+    tg.close();
+  };
 
   return (
     <div>
-      <Header />
-      <button onClick={() => setAsd(!asd)}>Нажми</button>
-      {asd === true ? <>Ты пидор</> : <></>}
+      work<button onClick={onClose}>Закрыть</button>
     </div>
   );
 };
