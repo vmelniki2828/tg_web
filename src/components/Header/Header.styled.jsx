6324ad9c0063b1a0cbceaba896e0ec2e5b4dd29f
Vlad Melnik <vmelniki2828@gmail.com>
2025-02-10 03:13:36 +0200

    edit

--- src/components/Header/Header.styled.jsx
+++ src/components/Header/Header.styled.jsx
@@ -92,12 +92,12 @@ export const DynamicImageContainer = styled.div`
 
   /* Стили для светлой темы */
   &.light {
-    background-color: rgb(255, 255, 255);
+    background-color: rgb(0, 0, 0);
   }
 
   /* Стили для тёмной темы */
   &.dark {
-    background-color: rgb(0, 0, 0);
+    background-color: rgb(255, 255, 255);
   }
 `;
 
