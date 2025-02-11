4c6a13bcea439088ad719433ebc3d579991aa8d7
Vlad Melnik <vmelniki2828@gmail.com>
2025-02-11 15:08:07 +0200

    edits

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -2,7 +2,15 @@ import { useEffect, useState } from 'react';
 import Header from '../../components/Header/Header';
 import light_bg from '../../images/light_bg.png';
 import dark_bg from '../../images/dark_bg.png';
-import { CircleStatus, MainContainer, StatusContainer } from './MainPage.styled';
+import {
+  BottomContainer,
+  CircleStatus,
+  ClearCasheBottom,
+  MainContainer,
+  QuestionCon,
+  StatusContainer,
+  UpdateBottom,
+} from './MainPage.styled';
 const tg = window.Telegram.WebApp;
 
 const MainPage = () => {
@@ -12,8 +20,15 @@ const MainPage = () => {
 
   return (
     <MainContainer>
-      <StatusContainer><CircleStatus />Connect</StatusContainer>
+      <StatusContainer>
+        <CircleStatus />
+        Connect<QuestionCon>?</QuestionCon>
+      </StatusContainer>
       <button onClick={onClose}>Закрыть</button>
+      <BottomContainer>
+        <ClearCasheBottom>Clear cache 53%</ClearCasheBottom>
+        <UpdateBottom>Update</UpdateBottom>
+      </BottomContainer>
     </MainContainer>
   );
 };
--- src/pages/MainPage/MainPage.styled.jsx
+++ src/pages/MainPage/MainPage.styled.jsx
@@ -1,9 +1,12 @@
 import styled from 'styled-components';
+import { BiQuestionMark } from 'react-icons/bi';
 
 export const MainContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  height: 100%;
+  justify-content: space-between;
 `;
 
 export const StatusContainer = styled.div`
@@ -14,6 +17,7 @@ export const StatusContainer = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-around;
+  margin-top: 40px;
 
   font-size: 20px;
   font-family: 'ProductSans', sans-serif;
@@ -23,11 +27,72 @@ export const StatusContainer = styled.div`
 `;
 
 export const CircleStatus = styled.div`
-    width: 10px; /* Размер кружочка */
-    height: 10px;
-    border-radius: 50%; /* Круглая форма */
-    background-color: #4caf50; /* Внутренний зеленый цвет */
-    border: 2px solid #81c784; /* Ободок более светлого зеленого цвета */
-    display: inline-block; /* Чтобы кружочек корректно отображался */
-    box-shadow: 0 0 8px rgba(76, 175, 80, 0.6); /* Эффект свечения */
+  width: 10px; /* Размер кружочка */
+  height: 10px;
+  border-radius: 50%; /* Круглая форма */
+  background-color: #4caf50; /* Внутренний зеленый цвет */
+  border: 2px solid #81c784; /* Ободок более светлого зеленого цвета */
+  display: inline-block; /* Чтобы кружочек корректно отображался */
+  box-shadow: 0 0 8px rgba(76, 175, 80, 0.6); /* Эффект свечения */
+`;
+
+export const QuestionCon = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 50%;
+  background-color: rgb(209, 216, 223);
+  width: 30px;
+  height: 30px;
+  font-size: 20px;
+  font-family: 'ProductSans', sans-serif;
+  color: rgb(255, 255, 255);
+  font-weight: bold;
+  line-height: 1.2;
+`;
+
+export const BottomContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  width: 90%; /* На всю ширину */
+  position: absolute;
+  bottom: 0;
+  margin-bottom: 41px;
+`;
+
+export const ClearCasheBottom = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  background-color: rgb(0, 0, 0);
+  width: 270px;
+  height: 65px;
+  border-radius: 32.5px;
+
+  font-size: 20px;
+  font-family: 'ProductSans', sans-serif;
+  color: rgb(255, 255, 255);
+  font-weight: bold;
+  line-height: 1.2;
+  cursor: pointer;
+  border: none;
+`;
+
+export const UpdateBottom = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  background-color: rgb(0, 122, 255);
+  width: 270px;
+  height: 65px;
+  border-radius: 32.5px;
+
+  font-size: 20px;
+  font-family: 'ProductSans', sans-serif;
+  color: rgb(255, 255, 255);
+  font-weight: bold;
+  line-height: 1.2;
+  cursor: pointer;
+  border: none;
 `;
