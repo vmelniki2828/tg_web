c6acf1b92f654063a542a0a377d8e26a00fe977c
Ponedilok Danyyl <danya253545@gmail.com>
2025-03-09 14:54:51 +0200

    statistic

--- src/pages/StatisticPage/StatisticPage.jsx
+++ src/pages/StatisticPage/StatisticPage.jsx
@@ -1,11 +1,32 @@
-import { StatisticPageConteiner } from "./StatisticPage.styled";
+import {
+  ButtonBlock,
+  CacheButton,
+  HPowerBlock,
+  InformationBlock,
+  NumberBlock,
+  NumberText,
+  ServersButton,
+  StatisticPageConteiner,
+  TotalNumberText,
+} from './StatisticPage.styled';
 
 const StatisticPage = () => {
   return (
-  <StatisticPageConteiner>
-    StatisticPage
-  </StatisticPageConteiner>
-  )
+    <StatisticPageConteiner>
+      <ButtonBlock>
+        <CacheButton>Clear cache</CacheButton>
+        <ServersButton>Selecting servers</ServersButton>
+      </ButtonBlock>
+      <InformationBlock>
+        <HPowerBlock>
+          <TotalNumberText>Total number of hPower</TotalNumberText>
+          <NumberBlock>
+            <NumberText>530 905</NumberText>
+          </NumberBlock>
+        </HPowerBlock>
+      </InformationBlock>
+    </StatisticPageConteiner>
+  );
 };
 
 export default StatisticPage;
--- src/pages/StatisticPage/StatisticPage.styled.jsx
+++ src/pages/StatisticPage/StatisticPage.styled.jsx
@@ -1,6 +1,84 @@
 import styled from 'styled-components';
 
+export const StatisticPageConteiner = styled.div``;
 
-export const StatisticPageConteiner = styled.div`
+export const ButtonBlock = styled.div`
+  margin-top: 34px;
 
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const CacheButton = styled.button`
+  background-color: rgb(23, 28, 38);
+
+  width: 161px;
+  height: 40px;
+
+  font-size: 20px;
+  font-family: 'ProductSans', sans-serif;
+  color: rgb(255, 255, 255);
+  font-weight: bold;
+  line-height: 1.2;
+
+  border-radius: 30px;
+
+  margin-right: 12px;
+`;
+
+export const ServersButton = styled.button`
+  background-color: rgb(0, 122, 255);
+  width: 161px;
+  height: 40px;
+
+  font-size: 20px;
+  font-family: 'ProductSans', sans-serif;
+  color: rgb(255, 255, 255);
+  font-weight: bold;
+  line-height: 1.2;
+
+  border: 0;
+  border-radius: 30px;
+`;
+
+export const InformationBlock = styled.div`
+  margin: 12px auto;
+  border-radius: 35px;
+  background-color: rgb(255, 255, 255);
+
+  width: 332px;
+  height: 259px;
+
+  padding: 21px 24px 25px 24px;
+`;
+
+export const HPowerBlock = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const TotalNumberText = styled.p`
+  font-size: 21px;
+  font-family: 'ProductSans', sans-serif;
+  color: rgb(0, 0, 0);
+  line-height: 1.905;
+
+  margin-right: 8px;
+`;
+
+export const NumberBlock = styled.div`
+  background-color: rgb(0, 122, 255);
+
+  width: 72px;
+  height: 24px;
+
+  border-radius: 15px;
+`;
+
+export const NumberText = styled.p`
+  font-size: 21px;
+  font-family: 'ProductSans', sans-serif;
+  color: #ffff;
+  line-height: 1.905;
 `;
